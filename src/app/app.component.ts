4573390947d4a6e1b454e44d9f5f903ceac1df0b
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-05-27 15:15:45 +0530

    Inital Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './todo';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angulartut';
+  newTask: string = '';
+  tasks: { title: string; isDone: boolean }[] = [];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ title: this.newTask, isDone: false });
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  toggleTaskStatus(index: number) {
+    this.tasks[index].isDone = !this.tasks[index].isDone;
+  }
 }
